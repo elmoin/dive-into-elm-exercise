76a297e84ae0e3d5afa34d2d689ec61604b77bac
sectore <info@websector.de>
2016-05-01 20:22:23 +0200

    add code + exercises

---
+++ Main.elm
@@ -0,0 +1,159 @@
+module Main (..) where
+
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Signal exposing (..)
+import StartApp.Simple as StartApp
+
+
+-- MODEL
+
+
+type alias Weather =
+  { day : String, max : Int, min : Int, description : String }
+
+
+type alias City =
+  String
+
+
+type alias Model =
+  { city : City, forecast : List Weather }
+
+
+
+-- forecast model data of Hamburg
+
+
+forecast_hh : Model
+forecast_hh =
+  { city = "Hamburg"
+  , forecast =
+      [ { day = "TUE", max = 19, min = 8, description = "Light Rain" }
+      , { day = "WED", max = 15, min = 4, description = "Mostly Sunny" }
+      , { day = "THU", max = 17, min = 6, description = "Sunny" }
+      , { day = "FRI", max = 20, min = 10, description = "Sunny" }
+      , { day = "SAT", max = 22, min = 11, description = "Sunny" }
+      , { day = "SUN", max = 22, min = 12, description = "Sunny" }
+      ]
+  }
+
+
+
+-- empty model data
+
+
+initalModel : Model
+initalModel =
+  { city = "", forecast = [] }
+
+
+
+-- UPDATE
+
+
+type Action
+  = Reset
+  | Show Model
+
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Reset ->
+      initalModel
+
+    Show forecast ->
+      forecast
+
+
+
+-- VIEW
+
+
+forecastListView : List Weather -> Html
+forecastListView forecast =
+  let
+    -- table header
+    header =
+      tr
+        []
+        [ th [] [ text "Day" ]
+        , th [] [ text "Max" ]
+        , th [] [ text "Min" ]
+        , th [] [ text "Detail" ]
+        ]
+
+    -- table content
+    list =
+      case forecast of
+        [] ->
+          []
+
+        detail ->
+          List.map forecastDetailView detail
+  in
+    table
+      []
+      [ thead
+          []
+          [ header ]
+      , tbody
+          []
+          list
+      ]
+
+
+forecastDetailView : Weather -> Html
+forecastDetailView detail =
+  tr
+    []
+    [ td [] [ text detail.day ]
+    , td [] [ text (toString detail.max ++ "°C") ]
+    , td [] [ text (toString detail.min ++ "°C") ]
+    , td [] [ text detail.description ]
+    ]
+
+
+cityView : City -> Html
+cityView city =
+  h1 [] [ text city ]
+
+
+view : Address Action -> Model -> Html
+view address model =
+  div
+    []
+    [ button
+        [ onClick address (Show forecast_hh) ]
+        [ text "Show forecast" ]
+    , button
+        [ onClick address Reset ]
+        [ text "Reset" ]
+    , cityView model.city
+    , forecastListView model.forecast
+    ]
+
+
+main : Signal Html.Html
+main =
+  StartApp.start { model = initalModel, view = view, update = update }
+
+
+
+-- --------------------------
+-- Execise 1:
+-- --------------------------
+-- Show forecast of Hamburg right after starting the app without the need to clicking a button
+-- --------------------------
+-- Execise 2:
+-- --------------------------
+-- Add a button "Berlin" to show forecast data of Berlin
+-- You can use the following wheater data of Berlin
+--   [ { day = "TUE", max = 18, min = 7, description = "Partly Cloudy" }
+--   , { day = "WED", max = 16, min = 4, description = "Cloudy" }
+--   , { day = "THU", max = 19, min = 6, description = "Sunny" }
+--   , { day = "FRI", max = 21, min = 10, description = "Mostly Sunny" }
+--   , { day = "SAT", max = 23, min = 11, description = "Mostly Sunny" }
+--   , { day = "SUN", max = 24, min = 12, description = "Mostly Sunny" }
+--   ]
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# dive-into-elm-exercise
-Exercise for everyone to dive into Elm (Elmoin Meetup May 2016)
+# Exercise for everyone to dive into Elm
+## Elmoin Meetup May 2016
+
+1. Fork https://github.com/elmoin/dive-into-elm-exercise.git
+
+2. Run app
+```
+git clone https://github.com/{username}/dive-into-elm-exercise.git
+cd dive-into-elm-exercise
+elm-package install
+elm-reactor
+```
+
+3. Exercises are defined in `Main.elm`
+
+4. Have fun :)
